3fdb38cb040224623efe1b5dab0a04e6427e476c
bespin-multi-cloud-3 <bespin.multi.cloud.3@gmail.com>
2026-05-19 17:51:54 +0900

    feat: finish ch07

---
+++ ch07/05 gallery-05/locals.tf
@@ -0,0 +1,25 @@
+locals {
+  org         = "tf-core"
+  project     = "gallery"
+  environment = var.env
+
+  namespace = "${local.org}-${local.project}-${local.environment}"
+
+  infra = {
+    lb = {
+      listener_port = var.infra_lb_listener_port
+    }
+
+    lt = {
+      service_port  = var.infra_lt_service_port
+      instance_type = var.infra_lt_instance_type
+    }
+
+    asg = {
+      max_size         = var.infra_asg_max_size
+      min_size         = var.infra_asg_min_size
+      desired_capacity = var.infra_asg_desired_capacity
+      deploy_version   = var.infra_asg_deploy_version
+    }
+  }
+}
